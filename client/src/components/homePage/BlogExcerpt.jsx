07f89ef2e4cbdd025b36a721afdca33e0332bc96
CvejovicZ95 <cvejovicz@gmail.com>
2024-10-25 13:15:37 +0200

    loaded blogs from backend with redux

--- client/src/components/homePage/BlogExcerpt.jsx
+++ client/src/components/homePage/BlogExcerpt.jsx
@@ -2,18 +2,22 @@ import React from "react";
 import { TimeAgo } from "./TimeAgo";
 import { ReactionButtons } from "./ReactionButtons";
 import { PostAuthor } from "./PostAuthor";
+import { useSelector } from "react-redux";
+import { selectBlogById } from "../../features/blogs/blogsSlice";
 
-export const BlogExcerpt = ({ title, content, id, timestamp, author }) => {
+export const BlogExcerpt = ({ blogId }) => {
+    const blog = useSelector( state => selectBlogById(state, blogId))
+
     return (
-        <article className="blog-excerpt">
-            <h2>{title}</h2>
-            <p>{content}</p>
+        <article key={blog._id} className="blog-excerpt">
+            <h2>{blog.title}</h2>
+            <p>{blog.content}</p>
             <h3>View Blog</h3>
             <div className="author-and-time">
-                <TimeAgo timestamp={timestamp} />
-                <PostAuthor author={author} />
+                <TimeAgo timestamp={blog.created_at} />
+                <PostAuthor userId={blog.userId} />
             </div>
-            <ReactionButtons />
+            <ReactionButtons blog={blog} />
         </article>
     );
 };
---
+++ client/src/features/blogs/blogsSlice.js
@@ -0,0 +1,46 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
+import { getBlogs } from "../../api/blogsApi";
+
+const initialState = {
+    blogs: [],
+    status: 'idle', // 'idle', | 'loading' | 'succeeded', | 'error' 
+    error: null
+};
+
+export const fetchBlogs = createAsyncThunk('blogs/fetchBlogs' , async (_, { rejectWithValue }) => {
+    try {
+        const response = getBlogs()
+        return response
+    } catch (error) {
+        return rejectWithValue(error.message)
+    }
+})
+
+const blogsSlice = createSlice({
+    name:'blogs',
+    initialState,
+    reducers:{},
+    extraReducers(builder) {
+        builder
+            .addCase(fetchBlogs.pending, (state) => {
+                state.status = 'pending'
+            })
+            .addCase(fetchBlogs.fulfilled, (state, action) => {
+                state.blogs = action.payload
+                state.status = 'succeeded'
+            })
+            .addCase(fetchBlogs.rejected, (state, action) => {
+                state.status = 'failed'
+                state.error = action.payload
+            })
+    }
+})
+
+export const selectAllBlogs = (state) => state.blogs.blogs
+export const getBlogsStatus = (state) => state.blogs.status
+export const getBlogsError = (state) => state.blogs.error
+
+export const selectBlogById = (state, blogId) => 
+    state.blogs.blogs.find(blog => blog._id === blogId);
+
+export default blogsSlice.reducer
